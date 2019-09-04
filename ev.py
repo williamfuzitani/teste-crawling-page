b55dce5e55c360a4a24d95c5adc50e687480c671
William <william@imac-de-william.senailondrina.com>
2019-09-04 16:44:22 -0300

    ev name links

--- ev.py
+++ ev.py
@@ -1,1 +1,18 @@
-response.css('div.brand-listing-container-frontpage a').getall()
+# response.css('div.brand-listing-container-frontpage a').getall()
+
+import scrapy
+
+class BrickSetSpider(scrapy.Spider):
+     name = "brickset_spider"
+     start_urls = ['https://www.evspecifications.com/']
+
+     def parse(self, response):
+          SET_SELECTOR = 'div.brand-listing-container-frontpage a'
+
+          for brickset in response.css(SET_SELECTOR):
+               NAME_SELECTOR = 'a ::text'
+               PAGE_SELECTOR = 'a ::attr(href)'
+               yield {
+                    'name': brickset.css(NAME_SELECTOR).extract_first(),
+                    'page': brickset.css(PAGE_SELECTOR).extract_first(),
+               }
